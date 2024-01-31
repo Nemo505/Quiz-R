8a8fcf720702babf815e2dbc74dcc6ff2d07240e
nemo505 <dahliahalesia8@gmail.com>
2024-01-31 10:48:02 +0630

    ui

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -31,55 +31,60 @@ const Quiz = () => {
     }, []);
 
     useEffect(() => {
-    if (question !== null) {
-        const shuffleOptions = () => {
-        const allOptions = [...question[index].incorrect_answers, question[index].correct_answer];
-        // Fisher-Yates Shuffle Algorithm
-        for (let i = allOptions.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [allOptions[i], allOptions[j]] = [allOptions[j], allOptions[i]];
-        }
-        setOptions(allOptions);
-        };
+        if (question !== null) {
+            const shuffleOptions = () => {
+                const allOptions = [...question[index].incorrect_answers, question[index].correct_answer];
+                // Fisher-Yates Shuffle Algorithm
+                for (let i = allOptions.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [allOptions[i], allOptions[j]] = [allOptions[j], allOptions[i]];
+                }
+                setOptions(allOptions);
+            };
 
-        shuffleOptions();
-    }
+            shuffleOptions();
+        }
     }, [question, index]);
 
     const handleNextQuestion = () => {
     }
   
   return (
-      <div className="grid place-items-center h-screen bg-purple-100 ">
-        <div className="container mx-{160px} py-16 ring-2 ring-purple-500 rounded-md bg-purple-300">
+      <div className="grid place-items-center h-screen  " style={{ background: 'linear-gradient(to right, #3d1223, #302b63, #3d1223)'}}>
+          <div className="w-300 py-16 ring-1 
+                            ring-purple-400 rounded-md
+                            bg-pink-300 bg-opacity-25 shadow-2xl
+                        ">
           {question ? (
-              <div>
+                <div>
                   {index < question.length ? (
-                        <div className="flex flex-col items-center">
-                              <div className="text-2xl font-semi 
-                                                text-white text-center 
-                                                mb-4 bg-purple-500 bg-opacity-50
-                                                p-4 ring-2 ring-purple-200
-                                                rounded-md shadow-lg
-                                            ">
-                                  {question[index].question}
-                              </div>
+                          <div className="flex flex-col items-center">
+                                <div className="text-2xl font-semi-bold
+                                                    text-white text-center 
+                                                    mb-16 bg-purple-500 bg-opacity-50
+                                                    p-4 ring-1 ring-purple-400
+                                                    rounded-xl shadow-lg m-4"
+                                    style={{ width: '500px', fontFamily: 'caveat', background: 'linear-gradient(to right,   #76448a ,  #d2b4de,  purple , #d2b4de, purple)' }}>
+                                    {question[index].question}
+                                </div>
+                              
                             <div className="grid grid-cols-2 gap-4">
                                 {options.map((option, index) => (
                                     <button
                                         className='bg-gradient-to-r 
-                                                    from-purple-300 to-purple-700 
-                                                    hover:from-purple-700 
-                                                    hover:to-purple-300 
-                                                    text-white font-bold 
+                                                    from-fuchsia-400 to-fuchsia-800 
+                                                    hover:from-fuchsia-700 
+                                                    hover:to-fuchsia-400 
+                                                    text-white font-semi-bold 
                                                     py-2 px-4 rounded-md 
                                                     transition duration-300 ease-in-out 
                                                     focus:outline-none 
-                                                    focus:shadow-outline-purple 
-                                                    active:bg-purple-800
-                                                    ring-1 ring-purple-600
+                                                    focus:shadow-outline-fuchsia 
+                                                    active:bg-fuchsia-800
+                                                    ring-1 ring-purple-400
                                                     flex
                                                     justify-start'
+                                        style={{ fontFamily: 'cormorant-garamond' }}
                                         key={index}
                                         onClick={() => handleOptionClick(option)}
                                     >
@@ -88,7 +93,12 @@ const Quiz = () => {
                                 ))}
                             </div>
                             <button
-                                className="mt-4 p-2 bg-purple-800 
+                                className="mt-12 p-2 bg-gradient-to-r 
+                                                    from-purple-700 to-purple-800 
+                                                    hover:from-purple-700 
+                                                    hover:to-purple-700 
+                                                    ring-1 ring-purple-600
+                                                    shadow-lg
                                             text-white rounded-md"
                                 onClick={handleNextQuestion}
                             >
